2d019a21c2ee3e3a5f94ead54663a23f1dbcde72
admin <admin@mac.home>
2026-08-05 14:01:04 +0200

    Ajout génération PDF

--- server.js
+++ server.js
@@ -1,16 +1,1 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "template.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`🚀 Serveur lancé sur le port ${PORT}`);
-});
+Unsupported Media Type
