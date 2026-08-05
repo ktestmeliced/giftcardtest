ee0c144193480a9f6fc6fa72c612c7ce14d74c4b
admin <admin@mac.home>
2026-08-05 13:54:20 +0200

    Fix Railway port

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require("path");
 
 const app = express();
 
-// Sert tous les fichiers statiques (CSS, JS, images...)
 app.use(express.static(__dirname));
 
-// Affiche le template
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "template.html"));
 });
 
-// Railway fournit un port automatiquement
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
